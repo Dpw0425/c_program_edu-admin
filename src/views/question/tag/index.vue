393ccf73b3585bf0287c7f484d457429d7c7cea0
tich0425 <2981342943@qq.com>
2025-04-29 22:10:08 +0800

    feat: added static UI for question category management

--- src/views/question/tag/index.vue
+++ src/views/question/tag/index.vue
@@ -2,20 +2,20 @@
   <el-card class="box-card">
     <el-button type="primary" size="default" icon="Plus">添加分类</el-button>
 
-    <el-table style="margin: 10px 0;" border>
-        <el-table-column prop="date" label="序号" width="80px" align="center" />
-        <el-table-column prop="name" label="分类名称" />
-        <el-table-column prop="#" label="最后更新时间" />
-        <el-table-column prop="address" label="分类管理" />
+    <el-table style="margin: 10px 0" border>
+      <el-table-column prop="date" label="序号" width="80px" align="center" />
+      <el-table-column prop="name" label="分类名称" />
+      <el-table-column prop="#" label="最后更新时间" />
+      <el-table-column prop="address" label="分类管理" />
     </el-table>
 
     <el-pagination
-        v-mode:current-page="pageNo"
-        :page-size="limit"
-        :page-count="pager"
-        background
-        layout="prev, pager, next, jumper, ->, total"
-        :total="total"
+      v-mode:current-page="pageNo"
+      :page-size="limit"
+      :page-count="pager"
+      background
+      layout="prev, pager, next, jumper, ->, total"
+      :total="total"
     />
   </el-card>
 </template>
