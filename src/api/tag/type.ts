947cd83e943ad23fefafd896971e704c756b1d37
tich0425 <2981342943@qq.com>
2025-05-05 16:02:15 +0800

    feat: added data exchange for tag management

---
+++ src/api/tag/type.ts
@@ -0,0 +1,15 @@
+import type { ResponseData } from "../type"
+
+interface tagItem {
+    id: number
+    name: string
+    count: number
+}
+export type TagList = tagItem[] 
+
+export interface tagListResponseData extends ResponseData {
+    data: {
+        tag_list: TagList
+        total: number
+    }
+}
--- src/views/question/tag/index.vue
+++ src/views/question/tag/index.vue
@@ -2,17 +2,21 @@
   <el-card class="box-card">
     <el-button type="primary" size="default" icon="Plus">添加分类</el-button>
 
-    <el-table style="margin: 10px 0" border>
-      <el-table-column prop="date" label="序号" width="80px" align="center" />
+    <el-table style="margin: 10px 0" border :data="tagList">
+      <el-table-column label="序号" width="80px" align="center" type="index" />
       <el-table-column prop="name" label="分类名称" />
-      <el-table-column prop="#" label="最后更新时间" />
-      <el-table-column prop="address" label="分类管理" />
+      <el-table-column prop="count" label="题目数量" />
+      <el-table-column label="分类管理">
+        <template #="{row, $index}">
+          <el-button type="warning" size="small" icon="Edit"></el-button>
+          <el-button type="danger" size="small" icon="Delete"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
 
     <el-pagination
       v-mode:current-page="pageNo"
       :page-size="limit"
-      :page-count="pager"
       background
       layout="prev, pager, next, jumper, ->, total"
       :total="total"
@@ -21,15 +25,34 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reqTagList } from '@/api/tag'
+import type { TagList, tagListResponseData } from '@/api/tag/type'
+import { onMounted, ref } from 'vue'
 // 当前页码
 let pageNo = ref<number>(1)
 // 每页展示条数
 let limit = ref<number>(5)
-// 总页数
-let pager = ref<number>(10)
 // 总条数
-let total = ref<number>(50)
+let total = ref<number>(0)
+
+let tagList = ref<TagList>([])
+
+const getTagList = async () => {
+  let result: tagListResponseData = await reqTagList(pageNo.value, limit.value, null)
+  if (result.code == 200) {
+    total.value = result.data.total
+    tagList.value = result.data.tag_list.map(item => {
+      return {
+        ...item,
+        count: item.count != null ? item.count: 0
+      }
+    })
+  }
+}
+
+onMounted(() => {
+  getTagList()
+})
 </script>
 
 <style scoped></style>
