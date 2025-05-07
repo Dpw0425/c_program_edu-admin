76af0de32f4ce06e95029f4084e31c4ea8cd96ab
tich0425 <2981342943@qq.com>
2025-05-07 16:07:24 +0800

    feat: completed the static UI of the user management module

--- src/api/authorization/type.ts
+++ src/api/authorization/type.ts
@@ -1,35 +1,35 @@
-import type { ResponseData } from "../type";
+import type { ResponseData } from '../type'
 
 interface userItem {
-    user_id: number
-    user_name: string
-    student_id?: string
-    grade: number
-    status: number
+  user_id: number
+  user_name: string
+  student_id?: string
+  grade: number
+  status: number
 }
 
 export type UserList = userItem[]
 
 export interface userListResponseData extends ResponseData {
-    data?: {
-        user_list: UserList
-        total: number
-    }
+  data?: {
+    user_list: UserList
+    total: number
+  }
 }
 
 interface adminItem {
-    id: number
-    user_name: string
-    teacher_id: string
-    permission: string
-    status: number
+  id: number
+  user_name: string
+  teacher_id: string
+  permission: string
+  status: number
 }
 
 export type AdminList = adminItem[]
 
 export interface adminListResponseData extends ResponseData {
-    data?: {
-        admin_list: AdminList
-        total: number
-    }
+  data?: {
+    admin_list: AdminList
+    total: number
+  }
 }
